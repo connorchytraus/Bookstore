fb98d2ee36eb0caa8609653a6e129bd6f9f1497f
Connor Chytraus <connorchyt@gmail.com>
2023-03-06 16:39:47 -0700

    jkl

--- Bookstore/Controllers/HomeController.cs
+++ Bookstore/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace Bookstore.Controllers
         {
             repo = temp;
         }
-        public IActionResult Index(int pageNum = 1)
+        public IActionResult Index(string bookCategory, int pageNum = 1)
         {
             int pageSize = 10;
             int numBooks = repo.Books.Count();
@@ -24,13 +24,17 @@ namespace Bookstore.Controllers
             var x = new BooksViewModel
             {
                 Books = repo.Books
+                .Where(x => x.Category == bookCategory || bookCategory == null)
                 .OrderBy(x => x.Title)
                 .Skip((pageNum - 1) * pageSize)
                 .Take(pageSize),
 
                 PageInfo = new PageInfo
                 {
-                    TotalNumBooks = numBooks,
+                    TotalNumBooks = 
+                    (bookCategory == null
+                        ? repo.Books.Count()
+                        : repo.Books.Where(x => x.Category == bookCategory).Count()),
                     BooksPerPage = pageSize,
                     CurrentPage = pageNum
                 }
--- Bookstore/Startup.cs
+++ Bookstore/Startup.cs
@@ -36,6 +36,11 @@ namespace Bookstore
             });
 
             services.AddScoped<IBookstoreRepository, EFBookstoreRepository>();
+
+            services.AddRazorPages();
+
+            services.AddDistributedMemoryCache();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,12 +53,28 @@ namespace Bookstore
 
             //wwwroot
             app.UseStaticFiles();
-
+            app.UseSession();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
+                //endpoints for the filtering
+                endpoints.MapControllerRoute("categorypage",
+                    "{bookCategory}/Page{pageNum}",
+                    new { Controller = "Home", action = "Index" });
+
+                endpoints.MapControllerRoute(
+                   name: "Paging",
+                   pattern: "{pageNum}",
+                   defaults: new { Controller = "Home", action = "Index", pageNum = 1 });
+
+                endpoints.MapControllerRoute("category",
+                    "{bookCategory}",
+                    new { Controller = "Home", action = "Index", pageNum = 1 });
+               
                 endpoints.MapDefaultControllerRoute();
+
+                endpoints.MapRazorPages();
             });
         }
     }
